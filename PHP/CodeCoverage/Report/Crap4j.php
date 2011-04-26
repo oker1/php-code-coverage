61424af125beea0b1a4a81bb194eb848e3596963
pepov <pepov@ustream.tv>
2011-04-27 00:07:55 +0200

    implemented the bare minimum of the report to work with the crap4j hudson plugin

--- PHP/CodeCoverage/Report/Crap4j.php
+++ PHP/CodeCoverage/Report/Crap4j.php
@@ -7,6 +7,8 @@
  */
 class PHP_CodeCoverage_Report_Crap4j
 {
+	private $treshHold = 30;
+
     /**
      * @param  PHP_CodeCoverage $coverage
      * @param  string           $target
@@ -19,81 +21,25 @@ class PHP_CodeCoverage_Report_Crap4j
         $document->formatOutput = TRUE;
 
         $root = $document->createElement('crap_result');
-
         $document->appendChild($root);
 
         $project = $document->createElement('project', is_string($name) ? $name : '');
-
         $root->appendChild($project);
-
         $root->appendChild($document->createElement('timestamp', date('Y-m-d H:i:s', (int)$_SERVER['REQUEST_TIME'])));
 
+		$stats = $document->createElement('stats');
         $methodsNode = $document->createElement('methods');
 
-        $files    = $coverage->getSummary();
-        $packages = array();
+        $files = $coverage->getSummary();
 
-        $projectStatistics = array(
-          'files'               => 0,
-          'loc'                 => 0,
-          'ncloc'               => 0,
-          'classes'             => 0,
-          'methods'             => 0,
-          'coveredMethods'      => 0,
-          'conditionals'        => 0,
-          'coveredConditionals' => 0,
-          'statements'          => 0,
-          'coveredStatements'   => 0
-        );
-
-
-        /**
-         * <methods>
-    <method>
-      <package>
-        bancoagitar
-      </package>
-      <className>
-        BancoAgitar
-      </className>
-      <methodName>
-        &lt;init&gt;
-      </methodName>
-      <methodSignature>
-        ()V
-      </methodSignature>
-      <fullMethod>
-        public  void &lt;init&gt;()
-      </fullMethod>
-      <crap>
-        1.00
-      </crap>
-      <complexity>
-        1
-      </complexity>
-      <coverage>
-        100.00
-      </coverage>
-      <crapLoad>
-        0
-      </crapLoad>
-    </method>
-         */
+		$fullMethodCount = 0;
+		$fullCrapMethodCount = 0;
+		$fullCrapLoad = 0;
+		$fullCrap = 0;
 
         foreach ($files as $filename => $data) {
 
-            $namespace = 'global';
-
             if (file_exists($filename)) {
-                $fileStatistics = array(
-                  'classes'             => 0,
-                  'methods'             => 0,
-                  'coveredMethods'      => 0,
-                  'conditionals'        => 0,
-                  'coveredConditionals' => 0,
-                  'statements'          => 0,
-                  'coveredStatements'   => 0
-                );
 
                 $file = $document->createElement('file');
                 $file->setAttribute('name', $filename);
@@ -131,81 +77,39 @@ class PHP_CodeCoverage_Report_Crap4j
 
                     foreach ($_class['methods'] as $methodName => $method) {
 
+						$methodCount = 0;
+						$methodLines = 0;
+						$methodLinesCovered = 0;
+						$this->methodsDetails($method, $ignoredLines, $files, $filename, $classStatistics, $lines, $methodCount, $methodLines, $methodLinesCovered);
 
-                        $methodCount        = 0;
-                        $methodLines        = 0;
-                        $methodLinesCovered = 0;
-
-                        for ($i  = $method['startLine'];
-                             $i <= $method['endLine'];
-                             $i++) {
-                            if (isset($ignoredLines[$i])) {
-                                continue;
-                            }
-
-                            $add   = TRUE;
-                            $count = 0;
-
-                            if (isset($files[$filename][$i])) {
-                                if ($files[$filename][$i] != -2) {
-                                    $classStatistics['statements']++;
-                                    $methodLines++;
-                                }
-
-                                if (is_array($files[$filename][$i])) {
-                                    $classStatistics['coveredStatements']++;
-                                    $methodLinesCovered++;
-                                    $count = count($files[$filename][$i]);
-                                }
-
-                                else if ($files[$filename][$i] == -2) {
-                                    $add = FALSE;
-                                }
-                            } else {
-                                $add = FALSE;
-                            }
-
-                            $methodCount = max($methodCount, $count);
-
-                            if ($add) {
-                                $lines[$i] = array(
-                                  'count' => $count,
-                                  'type'  => 'stmt'
-                                );
-                            }
-                        }
-
+						$fullMethodCount++;
 
                         $coveragePercent = PHP_CodeCoverage_Util::percent(
                             $methodLinesCovered,
                             $methodLines
                         );
                         $crap = PHP_CodeCoverage_Util::crap($method['ccn'], $coveragePercent);
+						$fullCrap += $crap;
 
+						if ($crap >= $this->treshHold) {
+							$fullCrapMethodCount++;
+						}
+
+						$crapLoad = $this->getCrapLoad($crap, $method['ccn'], $coveragePercent);
+
+						$fullCrapLoad += $crapLoad;
 
                         $methodNode = $document->createElement('method');
 
                         $methodNode->appendChild($document->createElement('package', ''));
                         $methodNode->appendChild($document->createElement('className', $className));
                         $methodNode->appendChild($document->createElement('methodName', $methodName));
-                        $methodNode->appendChild($document->createElement('methodSignature', $method['signature']));
-                        $methodNode->appendChild($document->createElement('fullMethod', sprintf('%s(%s)', $methodName, $method['signature'])));
+						$methodNode->appendChild($document->createElement('methodSignature', htmlspecialchars($method['signature'])));
+                        $methodNode->appendChild($document->createElement('fullMethod', htmlspecialchars($method['signature'])));
                         $methodNode->appendChild($document->createElement('crap', $crap));
                         $methodNode->appendChild($document->createElement('complexity', $method['ccn']));
                         $methodNode->appendChild($document->createElement('coverage', $coveragePercent));
-                        $methodNode->appendChild($document->createElement('crapLoad', 0));
-
-                        /*
-                         *   public int getCrapLoad(float crapThreshold) {
-    int crapLoad = 0;
-    if (getCrap() >= crapThreshold) {
-      int complexity = getComplexity();
-      float coverage = getCoverage();
-      crapLoad += complexity * (1.0 - coverage);
-      crapLoad += complexity / crapThreshold;
-    }
-    return crapLoad;
-  }*/
+                        $methodNode->appendChild($document->createElement('crapLoad', $crapLoad));
 
                         $methodsNode->appendChild($methodNode);
 
@@ -215,106 +119,20 @@ class PHP_CodeCoverage_Report_Crap4j
                             $classStatistics['coveredMethods']++;
                         }
                     }
-
-                    /*if (!empty($package['fullPackage'])) {
-                        $class->setAttribute(
-                          'fullPackage', $package['fullPackage']
-                        );
-                    }
-
-                    if (!empty($package['category'])) {
-                        $class->setAttribute(
-                          'category', $package['category']
-                        );
-                    }
-
-                    if (!empty($package['package'])) {
-                        $class->setAttribute(
-                          'package', $package['package']
-                        );
-                    }
-
-                    if (!empty($package['subpackage'])) {
-                        $class->setAttribute(
-                          'subpackage', $package['subpackage']
-                        );
-                    }*/
                 }
             }
         }
 
-        $root->appendChild($methodsNode);
-
-        $stats = $document->createElement('stats');
-
         $stats->appendChild($document->createElement('name', 'Method Crap Stats'));
 
-        $stats->appendChild($document->createElement('totalCrap', 0));
-        $stats->appendChild($document->createElement('crap', 0));
-        $stats->appendChild($document->createElement('median', 0));
-        $stats->appendChild($document->createElement('average', 0));
-        $stats->appendChild($document->createElement('stdDev', 0));
-        $stats->appendChild($document->createElement('methodCount', 0));
-        $stats->appendChild($document->createElement('crapMethodCount', 0));
-        $stats->appendChild($document->createElement('crapMethodPercent', 0));
-        $stats->appendChild($document->createElement('crapLoad', 0));
-        $stats->appendChild($document->createElement('crapThreshold', 0));
-        $stats->appendChild($document->createElement('crapLoad', 0));
-        $stats->appendChild($document->createElement('globalCraploadAverage', 0));
-        $stats->appendChild($document->createElement('globalCrapMethodAverage', 0));
-        $stats->appendChild($document->createElement('globalTotalMethodAverage', 0));
-        $stats->appendChild($document->createElement('globalAverageDiff', 0));
-        $stats->appendChild($document->createElement('globalCraploadAverageDiff', 0));
-        $stats->appendChild($document->createElement('globalCrapMethodAverageDiff', 0));
-        $stats->appendChild($document->createElement('globalTotalMethodAverageDiff', 0));
+		$stats->appendChild($document->createElement('methodCount', $fullMethodCount));
+		$stats->appendChild($document->createElement('crapMethodCount', $fullCrapMethodCount));
+        $stats->appendChild($document->createElement('crapLoad', $fullCrapLoad));
+		$stats->appendChild($document->createElement('totalCrap', $fullCrap));
+		$stats->appendChild($document->createElement('crapMethodPercent', 100 * $fullCrapMethodCount / $fullMethodCount));
 
         $root->appendChild($stats);
-
-        /*
-
-        $metrics = $document->createElement('metrics');
-
-        $metrics->setAttribute('files', $projectStatistics['files']);
-        $metrics->setAttribute('loc', $projectStatistics['loc']);
-        $metrics->setAttribute('ncloc', $projectStatistics['ncloc']);
-        $metrics->setAttribute('classes', $projectStatistics['classes']);
-        $metrics->setAttribute('methods', $projectStatistics['methods']);
-
-        $metrics->setAttribute(
-          'coveredmethods', $projectStatistics['coveredMethods']
-        );
-
-        $metrics->setAttribute(
-          'conditionals', $projectStatistics['conditionals']
-        );
-
-        $metrics->setAttribute(
-          'coveredconditionals', $projectStatistics['coveredConditionals']
-        );
-
-        $metrics->setAttribute(
-          'statements', $projectStatistics['statements']
-        );
-
-        $metrics->setAttribute(
-          'coveredstatements', $projectStatistics['coveredStatements']
-        );
-
-        $metrics->setAttribute(
-          'elements',
-          $projectStatistics['conditionals'] +
-          $projectStatistics['statements']   +
-          $projectStatistics['methods']
-        );
-
-        $metrics->setAttribute(
-          'coveredelements',
-          $projectStatistics['coveredConditionals'] +
-          $projectStatistics['coveredStatements']   +
-          $projectStatistics['coveredMethods']
-        );
-
-        $project->appendChild($metrics);*/
+		$root->appendChild($methodsNode);
 
         if ($target !== NULL) {
             if (!is_dir(dirname($target))) {
@@ -326,4 +144,56 @@ class PHP_CodeCoverage_Report_Crap4j
             return $document->saveXML();
         }
     }
+
+	private function methodsDetails($method, $ignoredLines, $files, $filename, $classStatistics, $lines, &$methodCount, &$methodLines, &$methodLinesCovered)
+	{
+		for ($i = $method['startLine'];
+			$i <= $method['endLine'];
+			$i++) {
+			if (isset($ignoredLines[$i])) {
+				continue;
+			}
+
+			$add = TRUE;
+			$count = 0;
+
+			if (isset($files[$filename][$i])) {
+				if ($files[$filename][$i] != -2) {
+					$classStatistics['statements']++;
+					$methodLines++;
+				}
+
+				if (is_array($files[$filename][$i])) {
+					$classStatistics['coveredStatements']++;
+					$methodLinesCovered++;
+					$count = count($files[$filename][$i]);
+				}
+
+				else if ($files[$filename][$i] == -2) {
+					$add = FALSE;
+				}
+			} else {
+				$add = FALSE;
+			}
+
+			$methodCount = max($methodCount, $count);
+
+			if ($add) {
+				$lines[$i] = array(
+					'count' => $count,
+					'type' => 'stmt'
+				);
+			}
+		}
+	}
+
+	public function getCrapLoad($crapValue, $cyclomaticComplexity, $coveragePercent)
+	{
+    	$crapLoad = 0;
+    	if ($crapValue > $this->treshHold) {
+      		$crapLoad += $cyclomaticComplexity * (1.0 - $coveragePercent / 100);
+      		$crapLoad += $cyclomaticComplexity / $this->treshHold;
+		}
+    	return $crapLoad;
+	}
 }
